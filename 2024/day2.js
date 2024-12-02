3fdf52a38b3e83c927214df9ecf1d1e323fdd064
TubTubTub <tobykwmok@gmail.com>
2024-12-02 15:20:06 +0000

    finished day 2 WHY IS JS SORT NOT AUTO SUPPORT NUMBERS AHHHH

--- 2024/day2.js
+++ 2024/day2.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-let input = fs.readFileSync('inputs/day2 copy.txt', { encoding: 'utf8', flag: 'r' })
+let input = fs.readFileSync('inputs/day2.txt', { encoding: 'utf8', flag: 'r' })
 input = input.toString().split('\n')
 
 const part1 = () => {
@@ -27,49 +27,66 @@ const part1 = () => {
 }
 
 const part2 = () => {
-    const result = input.filter((line) => {
-
-        const numbers = line.split(' ').map((num) => Number(num))
-        let increasing = (numbers[1] - numbers[0]) >= 0
-        let updated = false
-        
-        for (let i=1 ; i<numbers.length ; i++) {
-            const difference = numbers[i] - numbers[i-1]
-            if (
-                (difference < 0 && increasing) ||
-                (difference > 0 && !increasing) ||
-                (Math.abs(difference) < 1 || Math.abs(difference) > 3)
-            ) {
-                if (updated) {
-                    return false
-                } else {
-                    numbers.splice(i, 1)
-                    updated = true
-                }
-            }
-        }
-
-        if (!updated) {
-            return true
-        }
-
-        increasing = (numbers[1] - numbers[0]) >= 0
-        console.log(numbers, increasing)
-        
-        for (let i=1 ; i<numbers.length ; i++) {
-            const difference = numbers[i] - numbers[i-1]
-            if (
-                (difference < 0 && increasing) ||
-                (difference > 0 && !increasing) ||
-                (Math.abs(difference) < 1 || Math.abs(difference) > 3)
-            ) {
+    const arraysEqual = (array1, array2) => {
+        for (let i=0 ; i < array1.length ; i++) {
+            if (array1[i] !== array2[i]) {
                 return false
             }
         }
         return true
+    }
+    
+    const sortNum = (array, ascending) => {
+        if (ascending) {
+            array.sort((a, b) => a - b)
+        } else {
+            array.sort((a, b) => b - a)
+        }
+    }
+    
+    const orderCorrect = (array) => {
+        const ascending = [...array]
+        const descending = [...array]
+        sortNum(ascending, true)
+        sortNum(descending, false)
+        
+        return (arraysEqual(array, ascending) || arraysEqual(array, descending))
+    }
+    
+    const differenceCorrect = (array) => {
+        for (let i=1 ; i < array.length ; i++) {
+            const difference = Math.abs(array[i] - array[i-1])
+            if (difference < 1 || difference > 3) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    const validNumbers = (numbers) => {
+        return (orderCorrect(numbers) && differenceCorrect(numbers))
+    }
+
+    const result = input.filter((line) => {
+
+        const numbers = line.split(' ').map((num) => Number(num))
+
+        if (validNumbers(numbers)) {
+            return true
+        }
+
+        for (let i=0 ; i < numbers.length ; i++) {
+            const testNumbers = [...numbers]
+            testNumbers.splice(i, 1)
+
+            if (validNumbers(testNumbers)) {
+                return true
+            }
+        }
+        
+        return false
 
     }).length
+
     console.log(result)
 }
-
-part2()
--- 2024/tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-list2Count.get(key
