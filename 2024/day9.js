f237beebf038f665273177bb9b148a5adef2d287
TubTubTub <tobykwmok@gmail.com>
2024-12-10 12:52:40 +0000

    day 9

--- 2024/day9.js
+++ 2024/day9.js
@@ -1,4 +1,3 @@
-import exp from 'constants'
 import fs from 'fs'
 
 let input = fs.readFileSync('2024/inputs/day9.txt', { encoding: 'utf8', flag: 'r' })
@@ -24,9 +23,6 @@ const part1 = () => {
         if (expanded.at(-1) !== '.') {
             const numberToMove = expanded.at(-1)
             const dotIndex = expanded.indexOf('.')
-            if (dotIndex < 100) {
-                console.log(dotIndex)
-            }
             expanded[dotIndex] = numberToMove
             expanded = expanded.slice(0, -1)
         } else {
@@ -37,15 +33,36 @@ const part1 = () => {
     expanded = expanded.concat(Array(originalLength).fill('.').slice(0, originalLength))
     let result = 0
     let i = 0
+
     while (expanded[i] !== '.') {
         result += Number(expanded[i]) * i
         i += 1
     }
+
+    console.log(result)
 }
 
 const part2 = () => {
+    const findSpace = (number, limit) => {
+        let streak = 0
+
+        for (let i=0 ; i < limit ; i++) {
+            if (expanded[i] === '.') {
+                streak += 1
+
+                if (streak === number) {
+                    return i - streak + 1
+                }
+
+            } else {
+                streak = 0
+            }
+        }
+
+        return null
+    }
+
     let expanded = []
-    const sizeMap = new Map()
     let runningSum = 0
 
     for (let i=0 ; i < input.length ; i++) {
@@ -58,52 +75,27 @@ const part2 = () => {
             for (let j=0 ; j < num ; j++) {
                 expanded.push('.')
             }
-            const indexes = sizeMap.get(num) || []
-            sizeMap.set(num, [...indexes, runningSum])
         }
         runningSum += Number(num)
-    }
-
-
-
-    const findSpace = (number, limit) => {
-        let streak = 0
-
-        for (let i=0 ; i < limit ; i++) {
-            if (expanded[i] === '.') {
-                streak += 1
-
-                if (streak === number) {
-                    return i - streak + 1
-                }
-            } else {
-                streak = 0
-            }
-        }
-
-        return null
     }
 
     let i = expanded.length - 1
     let current = null
     let length = 0
-    console.log('BASE' ,expanded)
+
     while (i >= -1) {
         if (expanded[i] !== current) {
             if (current !== '.') {
-                const insertIndex = findSpace(length, i)
+                const insertIndex = findSpace(length, i + 1)
 
                 if (insertIndex) {
-                    console.log('free space', insertIndex, length)
                     for (let j=0 ; j < length ; j++) {
-                        // console.log('inserting', length, current, 'at', insertIndex + j, 'replacing at', i + j)
-                        console.log(expanded)
-                        expanded[i + j] = '.'
+                        expanded[i + j + 1] = '.'
                         expanded[insertIndex + j] = current
                     }
                 }
-                
             }
+
             current = expanded[i]
             length = 1
         } else {
@@ -112,37 +104,17 @@ const part2 = () => {
 
         i -= 1
     }
+    
+    let result = 0
 
-    console.log(expanded)
+    for (const [index, item] of expanded.entries()) {
+        if (item !== '.') {
+            result += Number(item) * index
+        }
+    }
+
+    console.log(result)
 }
 
-    // for (let i=expanded.length - 1 ; i >= 0 ; i--) {
-    //     const numToMove = expanded[i]
-
-    //     let candidate = Infinity
-    //     let candidateSize = null
-
-    //     for (let j=1 ; j <= numToMove.length ; j++) {
-    //         const availablePos = sizeMap.get(j) || []
-            
-    //         if (availablePos.length > 0 && availablePos[0] < candidate) {
-    //             candidate = availablePos[0]
-    //             candidateSize = j
-    //         }
-    //     }
-
-    //     if (candidate === Infinity) {
-    //         continue
-    //     }
-
-    //     expanded[i] = '.'
-
-    //     for (let j=0 ; j < numToMove.length ; j++) {
-    //         expanded[candidate + j] = numToMove
-    //     }
-
-    //     candi
-
-    //     const remainder = numToMove.length - candidateSize
-
+part1()
 part2()
--- 2024/s.py
+++ 2024/s.py
@@ -6,34 +6,47 @@ storage = []
 id_count = 0
 for i in range(len(disk_map)):
     if is_free_space:
-        storage += ['.'] * int(disk_map[i])
+        storage.append((-1, int(disk_map[i])))
 
     else:
-        storage += [str(id_count)] * int(disk_map[i])
+        storage.append((id_count, int(disk_map[i])))
         id_count += 1
+        
     is_free_space = not is_free_space
 
-head = storage.index('.')
-
-tail = len(storage) - 1
+tail = 0
 for i in range(len(storage) - 1, -1, -1):
-    if storage[i] != '.':
+    if storage[i][0] != -1:
         tail = i
         break
 
-while head < tail:
-    storage[head] = storage[tail]
-    storage[tail] = '.'
-    while storage[head] != '.':
-        head += 1
-    while storage[tail] == '.':
+while tail > 0:
+    identifier, length = storage[tail]
+    for i in range(0, tail):
+        if storage[i][0] == -1 and storage[i][1] >= length:
+            diff = storage[i][1] - length
+            storage[i] = (-1, diff)
+            storage.insert(i, (identifier, length))
+            tail += 1
+            storage[tail] = (-1, length)
+            break
+
+    tail -= 1
+
+    while tail >= 0 and storage[tail][0] == -1:
         tail -= 1
 
-checksum = 0
-print(storage[0:100])
 
-for i in range(len(storage)):
-    if storage[i].isnumeric():
-        checksum += int(storage[i]) * i
+s = []
+for symbol, length in storage:
+    if symbol == -1:
+        s += [-1] * length
     else:
-        break
+        s += [symbol] * length
+
+checksum = 0
+for i in range(len(s)):
+    if s[i] != -1:
+        checksum += i * s[i]
+
+print(checksum)
