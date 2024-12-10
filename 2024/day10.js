dc8b37b27c1cbd7dee37950ccd9611d146095af4
TubTubTub <tobykwmok@gmail.com>
2024-12-10 14:47:54 +0000

    d

--- 2024/day10.js
+++ 2024/day10.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-let input = fs.readFileSync('2024/inputs/day10.txt', { encoding: 'utf8', flag: 'r' })
+let input = fs.readFileSync('inputs/day10.txt', { encoding: 'utf8', flag: 'r' })
 input = input.toString().split('\n')
 input = input.map((line) => {
     line = line.trim()
